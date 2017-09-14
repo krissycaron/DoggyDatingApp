097e2cf804607708abcc6c79f6d4de44e663adaa
Kristin Caron <krissycaron@gmail.com>
2017-09-14 11:50:21 -0500

    randomly generating users

--- Controllers/RandomGenerateUserController.cs
+++ Controllers/RandomGenerateUserController.cs
@@ -1,64 +1,44 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using serverSideCapstone.Data;
 using serverSideCapstone.Models;
 
 namespace serverSideCapstone.Controllers
 {
-    public class RandomGenerateUserController: ApplicationUser
+    public class RandomGenerateUserController: Controller
     {
-        // Get all user Query Database. 
-       
-        // public List<Users> ListOfUsers()
-        // {
-        //     try
-        //     {
-        //         OpenConnection();
-        //         (Select username 
-        //         From AspNetUsers;)
-        //     }
-        // }
-        //Get a List of users. 
-    
-    
-    
-    
-    //  public List<Object> getObjectsFromDatabase()
-        // {
-        //     try
-        //     {
-        //         OpenConnection();
-        //         // sql query
-        //         // Datareader
-        //         // sqlcommand
-        //         // return list van objecten , objecten veranderd naar jouw wens van data.
-        //         CloseConnection();
-        //     }
-        //     catch (Exception)
-        //     {
-        //         throw;
-        //     }
-        //     return new List<object>();
-        // }
+        private readonly ApplicationDbContext _context;
 
+        private readonly UserManager<ApplicationUser> _userManager;
 
+        public RandomGenerateUserController(ApplicationDbContext ctx, UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+            _context = ctx;
+        }
 
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+        public async Task<IActionResult> Index(string searchString, bool LocalDelivery)
+        {
+            // Get all user Query Database. 
+            var id = await GetCurrentUserAsync();
+            //Get a List of users, MINUS logged in user
+            var ListOfUsers = _context.ApplicationUser
+            .Where(cu => cu != id)
+            .ToList();
+            // currentUserDisplayed = GetRandomuser out of able list of users 
+            Random random = new Random();
+            var GetRandomuser = random.Next(ListOfUsers.Count);
 
-
-
-
-
-
-
-
-
-        // var currentUserDisplayed = GetRandomuser out of able list of users 
+            return View(ListOfUsers);
+        }
         // Genearate a random # 
         // GEt user out of the list. 
-
-    //   ApplicationUser randomlySelectedUser = new randomlySelectedUser();
-    //     foreach (ApplicationUser randomlySelectedUser in ListofUsers )
-    //     {
-    //         return 
-    //     }
         
     }
 }
